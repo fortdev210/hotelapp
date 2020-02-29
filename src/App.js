6a8f87f24fbfb94f01552117160a2ea16371bcb7
fortdev210 <sharp.blader2050@gmail.com>
2020-02-29 23:54:24 +0100

    Routes added in separate file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-
-import Home from './pages/Home';
-import Rooms from './pages/Rooms';
-import SingleRoom from './pages/SingleRoom';
-import Error from './pages/Error';
-
-import { Route, Switch} from 'react-router-dom';
-
 import Navbar from './components/navbarComponent/Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route exact path="/rooms" component={ Rooms } />
-        <Route exact path="/rooms/:slug" component={ SingleRoom } />
-        <Route component={ Error } />
-      </Switch>
+      <Routes />
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
+import { Route, Switch} from 'react-router-dom';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/rooms" component={ Rooms } />
+      <Route exact path="/rooms/:slug" component={ SingleRoom } />
+      <Route component={ Error } />
+    </Switch>
+  );
+}
+
+export default Routes;
