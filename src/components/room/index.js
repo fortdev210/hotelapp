261c8a2d61950f2c9f827cc4696729e202bc3a9c
fortdev210 <sharp.blader2050@gmail.com>
2020-03-02 15:12:24 +0100

    added fucntionality in rooms component

--- src/components/room/index.js
+++ src/components/room/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import defaultImage from '../../assets/images/file-missing.png';
 
+import './room.css';
+
 function Room({ room }) {
   const { name, slug, images, price } = room;
 
   return (
-    <article>
-      <div className="image-container">
+    <article className="room">
+      <div className="img-container">
         <img src={ images[0] || defaultImage } alt="single room" />
+        <div className="price-top">
+          <h6> ${price} </h6>
+          <p> per night </p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
       </div>
+      <p className="room-info"> {name} </p>
     </article>
   );
 }
