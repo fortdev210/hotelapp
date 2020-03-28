34fa401fd3835c1c6a9a9e8ebc8865148222ce18
fortdev210 <sharp.blader2050@gmail.com>
2020-03-28 21:10:44 +0100

    updated the page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -17,13 +17,13 @@ class SingleRoom extends Component {
   }
   static contextType = RoomContext;
 
-  // below lifecycle is canbe used to fetch data from API in this case.
+  // below lifecycle can be used to fetch data from API in this case.
   // componentDidMount() {}
 
   render() {
     const { getRoom } = this.context;
     const room = getRoom(this.state.slug);
-    if (!room) {
+    if ( !room ) {
       return (
         <div className="error">
           <h3> Room could not be found... </h3>
@@ -34,11 +34,50 @@ class SingleRoom extends Component {
 
     const { name, description, capacity, size, price, extras, breakfast, pets, images } = room;
     return (
-      <StyledHero img={images[0] || this.state.defaultBcg}>
-        <Banner title={`${name} room`}>
-          <Link to='/rooms' className="btn-primary"> back to rooms </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={ images[0] || this.state.defaultBcg }>
+          <Banner title={ `${name} room` }>
+            <Link to='/rooms' className="btn-primary"> back to rooms </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            { images.map(( item, index ) => {
+              return <img key={index} src={item} alt={name} />
+            }) }  
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3> Details </h3>
+              <p> {description} </p>
+            </article>
+            <article className="info">
+              <h3> Info </h3>
+              <h6> Price : ${price} </h6>
+              <h6> Size : ${size} SQFT </h6>
+              <h6>
+                Max Capacity : {
+                  capacity > 1 ? `${capacity} people` : `${capacity} person`
+                } 
+              </h6>
+              <h6>
+                { pets ? "pets allowed" : "no pets allowed"}
+              </h6>
+              <h6>
+                { breakfast && "free breakfast included"}
+              </h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h4> Extras </h4>
+          <ul className="extras">
+            { extras.map(( item, index ) => {
+              return <li key={index} > - {item} </li>
+            }) }
+          </ul>
+        </section>
+    </>
     );
   }
 }
